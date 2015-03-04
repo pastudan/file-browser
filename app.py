5b5ad5ed959062b1cb35d4707de802d7c30656bc
Dan Pastusek <dan@hexial.com>
2015-03-03 20:30:26 -0700

    initial commit. added README. added html placeholder. added GET method for retrieving files and method for accessing static files.

---
+++ app.py
@@ -0,0 +1,29 @@
+#!flask/bin/python
+from flask import Flask, send_from_directory
+import json
+from glob import glob
+
+app = Flask(__name__)
+
+
+# ideally these would go behind nginx or something..
+@app.route('/')
+def index():
+    return send_from_directory('static/', 'index.html')
+
+@app.route('/static/<path:path>')
+def send_js(path):
+    return send_from_directory('static', path)
+
+
+
+# API
+@app.route('/browser/api/v1.0/files/', methods=['GET'], defaults={'path': ''})
+@app.route('/browser/api/v1.0/files/<path:path>', methods=['GET'])
+def get_files(path):
+    path = '~/'+path+'/*'
+    print path
+    return json.dumps(glob(path))
+
+if __name__ == '__main__':
+    app.run(debug=True)
